4182ba116e4b13eee7f6c664387a63b5fb2cb37f
mohitekv <kajolmohite1996@gmail.com>
2020-03-09 11:48:02 +0530

    Display Calender of given Month and year

--- DataStructure/Calender.java
+++ DataStructure/Calender.java
@@ -1,13 +1,7 @@
 import java.util.Scanner;
+import demo.*;
 
 public class Calender {
-    public static int day(int month, int day, int year) {
-        int y = year - ((14 - month) / 12);
-        int x = y + y / 4 - y / 100 + y / 400;
-        int m = month + (12 * (14 - month) / 12) - 2;
-        int d = (day + x + (31 * m) / 12) % 7;
-        return d;
-    }
 
     public static boolean isLeapYear(int year) {
         if ((year % 400 == 0) || (year % 4 == 0) && (year % 100 != 0))
@@ -18,24 +12,23 @@ public class Calender {
 
     public static void main(String arg[]) {
         Scanner sc = new Scanner(System.in);
+        calender calender = new calender();
         System.out.println("Enter value of month and year");
         int month = sc.nextInt();
         int year = sc.nextInt();
-        String[] months = { "January", "February", "March", "April", "May", "June", "July", "August", "September",
+        String[] months = { " ", "January", "February", "March", "April", "May", "June", "July", "August", "September",
                 "October", "November", "December" };
-        int[] days = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+        int[] days = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
         // Checking for leap Year
         if (month == 2 && isLeapYear(year))
             days[month] = 29;
-        else
-            days[month] = 28;
         // month should be from 1 to 12
         // Year should be more than 4 digit
         if (month > 12 || year < 1000)
             System.out.println("Wrong input");
         else {
             System.out.println(" Su   M   Tu   W   Th   F  S");
-            int d = day(month, 1, year);
+            int d = calender.day(month, 1, year);
             for (int i = 0; i < d; i++)
                 System.out.print("    ");
             // printing calender
